b06e585a4780996e294b3bfbfc7f715eb2296f2a
alex <lo-w-rider@hotmail.com>
2023-08-03 21:42:22 -0600

    last changes

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -10,7 +10,7 @@ export default function Description(props: { description: any; setDescription: a
     setDescription(null);
   };
 
-
+//style={{ backgroundImage: `url(${'https://image.tmdb.org/t/p/original'+description.poster_path})` }}
   const [isHoveringHere, setIsHoveringHere] = useState(false);
   const handleMouseOver = () => {
     setIsHoveringHere(true);
--- src/pages/Home/Dashboard/Dashboard.tsx
+++ src/pages/Home/Dashboard/Dashboard.tsx
@@ -17,22 +17,47 @@ const Dashboard = memo(() => {
   let ArrayGen: gen[]=[];
   const [movies, setMovies] = useState(null);
   const [genders, setGenders] =  useState<gen[]>([]);
+  const [TypeMovies, setTypeMovies] = useState("now_playing");
+  const [pageCount, setPageCount] = useState(1);
 
- 
   useEffect(() => {
     GetGenders();
-    GetMovies();
+    GetMovies(TypeMovies);
   }, []);
+
+  
+  useEffect(() => {
+    GetMovies(TypeMovies,pageCount)
+  }, [pageCount]);
 
 
    const getFeatures =  (array: string | any[]) => {
+
+    if(genders.length==0) 
+    setGenders(ArrayGen);
+
     let categories = ""
     for (let i = 0; i < array.length; ++i) 
     {
+      
        const found = ArrayGen.find(obj => {
          return obj.id == array[i];
        });
+       if(found!=null && found!=undefined)
       categories+=found?.name+" / "
+    }
+
+    if(categories=="")
+    {
+      for (let i = 0; i < array.length; ++i) 
+      {
+        
+         const found = genders.find(obj => {
+           return obj.id == array[i];
+         });
+         if(found!=null && found!=undefined)
+        categories+=found?.name+" / "
+      }
     }
 
     return categories;
@@ -60,10 +85,12 @@ const Dashboard = memo(() => {
     });
   };
 
-  const GetMovies =  () => {
+  const GetMovies =  (type:string, page: string | number =1) => {
+      setTypeMovies(type);
+      console.log("****"+type);
     const options = {
       method: "GET",
-      url: "https://api.themoviedb.org/3/movie/now_playing?page=1",
+      url: `https://api.themoviedb.org/3/movie/${type}?page=${page}`,
       headers: {
         accept: "application/json",
         Authorization:
@@ -90,10 +117,43 @@ const Dashboard = memo(() => {
     });
   };
 
+  const classCheck1= TypeMovies=="now_playing" ? '-active' : "";
+  const classCheck2= TypeMovies=="popular" ? '-active' : "";
+  const classCheck3= TypeMovies=="top_rated" ? '-active' : "";
+  const classCheck4= TypeMovies=="upcoming" ? '-active' : "";
 
   return (
     <Authtemplate>
+    <div style={{textAlign: "center"}}>
+
+    <button className={`btn${classCheck1}`}
+    onClick={() => {
+      setPageCount(1)
+      setTypeMovies("now_playing")
+      GetMovies("now_playing")
+     }}
+    >Now Playing</button>
+
+    <button className={`btn${classCheck2}`} onClick={() => {
+         setPageCount(1)
+        setTypeMovies("popular")
+         GetMovies("popular")
+        }}>Popular</button>
+
+    <button  className={`btn${classCheck3}`}
+    onClick={() => {
+      setPageCount(1)
+      setTypeMovies("top_rated")
+      GetMovies("top_rated")
+     }}>Top rated</button>
+
+    <button className={`btn${classCheck4}`} onClick={() => {
+         setPageCount(1)
+      setTypeMovies("upcoming")
+      GetMovies("upcoming")
+     }}>Upcoming</button>
     <div>
+    </div>
 
     {movies ? (
           <Movies movies={movies} setMovies={setMovies} />
@@ -103,13 +163,12 @@ const Dashboard = memo(() => {
           </>
         )}
 
-      <button
-        onClick={() => {
-          navigate("/", { state: location.state });
-        }}
-      >
-        redirect
-      </button>
+    </div>
+    <div  style={{textAlign: "center"}}>
+    <button className="btn-page" disabled={pageCount == 1? true : false} onClick={() => setPageCount((c) => c - 1)}> &lt;</button>
+    <label>{pageCount}</label>
+    <button className="btn-page" disabled={pageCount >499 ? true : false} onClick={() => setPageCount((c) => c + 1)}
+    > &gt;</button>
     </div>
     </Authtemplate>
   );
